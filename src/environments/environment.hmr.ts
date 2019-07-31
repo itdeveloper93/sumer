d8903d6a85809346551b74efc758127df017faea
Azamat Rasulov <developer@evomedia.pro>
2019-07-31 15:18:46 +0500

    Fix HMR environment declaration

--- src/environments/environment.hmr.ts
+++ src/environments/environment.hmr.ts
@@ -6,6 +6,8 @@ export const environment = {
         blacklistedRoutes: ['localhost:4200/auth']
     },
     API: {
-        URL: 'https://51.145.98.38/api/'
+        URL: 'https://51.145.98.38/api/',
+        LOGIN: 'https://51.145.98.38/api/Account/Login',
+        REFRESH_TOKEN: 'https://51.145.98.38/api/Account/RefreshToken'
     }
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,11 @@
 export const environment = {
     production: true,
+    hmr: false,
     JWT: {
         whitelistedDomains: ['http://sumr.evomedia.pro'],
         blacklistedRoutes: ['http://sumr.evomedia.pro/auth']
     },
     API: {
-        URL: 'https://51.145.98.38/api/'
-    },
-    hmr: false
         URL: 'https://51.145.98.38/api/',
         LOGIN: 'https://51.145.98.38/api/Account/Login',
         REFRESH_TOKEN: 'https://51.145.98.38/api/Account/RefreshToken'
